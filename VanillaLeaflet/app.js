8f77e52b543a6b389b0ecbab7bf6202a56f8cdb0
bridgetorr17 <bridgetorr1902@gmail.com>
2025-09-01 09:33:54 -0400

    HTML/CSS/JS leaflet map and marker

---
+++ VanillaLeaflet/app.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribtion: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    var marker = L.marker([51.505, -0.09]).addTo(map);
+    marker.bindPopup('Hello, I\'m a marker').openPopup();
+})
