fa3e0471173ceb008183efe81c9cade73da5289d
bridgetorr17 <bridgetorr1902@gmail.com>
2025-09-11 18:41:51 -0400

    backend setup for requests

--- server/index.js
+++ server/index.js
@@ -1,12 +1,24 @@
 import express from 'express';
 const app = express();
 
+import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config({path: '/../.env', override: true, quiet: true})
 
-app.use(express.static('public'));
+app.use(cors({
+  origin: 'http://localhost:5173',   // allow your front-end origin
+  credentials: true                // so cookies / credentials: 'include' works
+}));
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+app.post('/', (req, res) => {
+    console.log('we made it to the backend')
+
+    res.json({ message: 'hello from backend' });
+});
+        
 
 app.listen(8000, () => {
     console.log('server running on port 8000');
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import { useState, useEffect } from "react"
-import MapComp from "./MapComp"
-
 const App = () => {
 
+    const backendTest = async () => {
+        const message = 'message to the backend'
 
+        const res = await fetch ('http://localhost:8000/', {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({mess: message})
+        })
+
+        const result = await res.json();
+        console.log(result.message);
+    }
+    
+    backendTest();
+    console.log('just called the backend test')
 
     return (
         <>
-         
+         <span>hello</span>
         </>
     )
 }
