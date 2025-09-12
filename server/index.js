0488d2e1baf08291c2b33059c784ea96f5b2805b
bridgetorr17 <bridgetorr1902@gmail.com>
2025-09-12 17:28:34 -0400

    formatting reset password email

--- server/index.js
+++ server/index.js
@@ -24,23 +24,36 @@ const mj = mailjet.apiConnect(
 app.post('/', async (req, res) => {
     console.log('we made it to the backend')
 
+    const resetUrl = 'https://triplytravel.vercel.app/resetPassword'
+
     try{
         const request = await mj.post('send', {version: 'v3.1'}).request({
             Messages: [
                 {
                     From: {
                         Email: 'bridgetorr1902@gmail.com',
-                        Name: 'Default',
+                        Name: 'Triply Password Reset',
                     },
                     To: [
                         {
-                            Email: 'triplytravel@yahoo.com',
-                            Name: 'Triply Travel',   
+                            Email: 'bridgetorr1902@gmail.com',
+                            Name: 'Test User',   
                         }
                     ],
-                    Subject: 'test email',
-                    TextPart: 'Test',
-                    HTMLPart: '<a href="https://triplytravel.vercel.app/"> Triply!!</>',
+                    Subject: 'Reset your Triply Password',
+                    TextPart: 
+                        `You requested a password reset. Please click the link below to set a new password:\n\n` +
+                        `${resetUrl}\n\n` +
+                        `If you didn’t request this, you can safely ignore this email.`,
+                    HTMLPart:  
+                        `<strong>Reset your password</strong><br/><br/>` +
+                        `You requested a password reset for your Triply account.<br/>` +
+                        `Click the button below to reset your password:<br/><br/>` +
+                        `<a href="${resetUrl}" style="display:inline-block;padding:12px 24px;background-color:#1D4ED8;color:white;text-decoration:none;border-radius:4px;">Reset Password</a><br/><br/>` +
+                        `If the button doesn’t work, copy and paste the following link into your browser:<br/>` +
+                        `<a href="${resetUrl}">${resetUrl}</a><br/><br/>` +
+                        `If you did not request a password reset, no action is needed.<br/><br/>` +
+                        `Thanks,<br/>The Triply Team`,
                 }
             ]
         });
